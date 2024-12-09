6fc8582f16080f6ad22264e80ab5ff0670462be4
David Merchan <davidmerchan10@gmail.com>
2024-12-09 14:15:00 -0500

    - Implemented detekt and delete example files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,32 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.android.hilt) apply false
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    // Config detekt plugin
+    detekt {
+        // Set the detekt configuration from previous steps
+        config.setFrom(file("$rootDir/config/detekt-config.yml"))
+
+        // Build upon the default detekt configuration, instead of replacing it
+        buildUponDefaultConfig = true
+
+        // Do not activate all detekt rules
+        allRules = false
+
+        // Enable automatic correction of issues found by detekt
+        autoCorrect = true
+
+        // Run detekt in parallel mode for better performance
+        parallel = true
+    }
+
+    dependencies {
+        detektPlugins("io.nlopez.compose.rules:detekt:0.4.4")
+    }
 }
